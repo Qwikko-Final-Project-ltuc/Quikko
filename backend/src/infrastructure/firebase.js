256a03c69b59b2c1ae362664e184ab1f6be9ccc5
Thekra Qaqish <qaqishth@gmail.com>
2025-11-16 03:23:06 +0300

    Update firebase.js

--- backend/src/infrastructure/firebase.js
+++ backend/src/infrastructure/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 /**
  * @module FirebaseConfig
